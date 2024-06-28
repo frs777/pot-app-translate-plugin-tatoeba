0864ca679fd4038d588f6f94afde5a1c33085de4
Pylogmon <pylogmon@outlook.com>
2024-06-28 10:17:07 +0800

    Update main.js

--- main.js
+++ main.js
@@ -4,20 +4,20 @@ async function translate(text, from, to, options) {
     const res = await fetch(`https://tatoeba.org/eng/api_v0/search`, {
         method: 'GET',
         query:
-            {
-                "query": text,
-                "from": from,
-                "to": to,
-                "has_audio": "no",
-                "sort": "relevance"
-            }
+        {
+            "query": text,
+            "from": from,
+            "to": to,
+            "has_audio": "no",
+            "sort": "relevance"
+        }
     });
 
     if (res.ok) {
         let result = res.data;
         let final = { "sentence": [] };
         const { results } = result;
-        for (let i in results) {
+        for (let i of results) {
             let source = i.text;
             let target = "";
             let translations = o.translations;
@@ -30,6 +30,7 @@ async function translate(text, from, to, options) {
             let sentence = { "source": source, "target": target };
             final.sentence.push(sentence);
         }
+        return final;
     } else {
         throw `Http Request Error\nHttp Status: ${res.status}\n${JSON.stringify(res.data)}`;
     }
